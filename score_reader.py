6508c63b55421db5193569652ea19a5716c93f91
Matt Burgess <mburgess@digitalglobe.com>
2018-04-16 11:02:13 -0600

    first working version

--- score_reader.py
+++ score_reader.py
@@ -14,9 +14,26 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 blurred = cv2.GaussianBlur(gray, (5, 5), 0)
 edged = cv2.Canny(blurred, 75, 200)
 
-thresh = cv2.threshold(edged, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+
+if len(cnts) > 0:
+    cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
+    for c in cnts:
+        peri = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+        if len(approx) == 4:
+            docCnt = approx
+            break
+
+paper = four_point_transform(image, docCnt.reshape(4, 2))
+warped = four_point_transform(gray, docCnt.reshape(4, 2))
+
+thresh = cv2.threshold(warped, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+
 cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+
 questionCnts = []
 
 for c in cnts:
@@ -27,4 +44,35 @@ for c in cnts:
         questionCnts.append(c)
 
 questionCnts = contours.sort_contours(questionCnts, method="top-to-bottom")[0]
+firstdigit = contours.sort_contours(questionCnts[:6])[0]
+seconddigit = contours.sort_contours(questionCnts[6:])[0]
 
+for (q, i) in enumerate(np.arange(0, 6, 6)):
+        firstdigitsort = contours.sort_contours(firstdigit[i:i+6])[0]
+
+for (q, i) in enumerate(np.arange(0, 10, 10)):
+        seconddigitsort = contours.sort_contours(seconddigit[i:i+10])[0]
+
+bubbled = None
+for (j, c) in enumerate(firstdigitsort):
+    mask = np.zeros(thresh.shape, dtype="uint8")
+    cv2.drawContours(mask, [c], -1, 255, -1)
+    mask = cv2.bitwise_and(thresh, thresh, mask=mask)
+    total = cv2.countNonZero(mask)
+    if bubbled is None or total > bubbled[0]:
+        bubbled = (total, j)
+
+
+first = str(bubbled[1])
+
+bubbled = None
+for (j, c) in enumerate(seconddigitsort):
+    mask = np.zeros(thresh.shape, dtype="uint8")
+    cv2.drawContours(mask, [c], -1, 255, -1)
+    mask = cv2.bitwise_and(thresh, thresh, mask=mask)
+    total = cv2.countNonZero(mask)
+    if bubbled is None or total > bubbled[0]:
+        bubbled = (total, j)
+
+second = str(bubbled[1])
+print first + second
